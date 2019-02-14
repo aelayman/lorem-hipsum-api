607a681fa4b315d7822a6d76c35cd9592fc9e2a3
Ann Layman <annelayman@gmail.com>
2019-02-13 20:55:20 -0500

    set up env based port

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 8080
 
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
