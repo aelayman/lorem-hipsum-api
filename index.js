a68c447bfea428e4a75b6f089c5787e9ab83d6d2
Ann Layman <annelayman@gmail.com>
2019-02-13 19:29:43 -0500

    set up initial express app

---
+++ index.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
