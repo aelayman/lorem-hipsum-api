1abf7737b874a96ea6c62edff420b2004b5c1c99
Ann Layman <annelayman@gmail.com>
2019-02-17 18:24:34 -0500

    API MVP complete

--- server/index.js
+++ server/index.js
@@ -3,17 +3,30 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000; // this can be very useful if you deploy to Heroku!
-const db = require('./db'); 
+const db = require('./db');
+const {Word} = require('../server/db/models')
+const Sequelize = require('sequelize');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use(morgan('dev'));
 
-const words = ['hello', 'world', 'hi']
+// need to grab the number of words requested from the db and send back single paragraph
 
-app.get('/', (req, res) => res.status(200).send(words))
+app.get('/', (req, res) => {
+  // get num words from request
+  const numWords = Number(req.query.numWords);
+  Word.findAll({ order: Sequelize.literal('random()'), limit: numWords })
+  .then((wordRecords) => {
+    const words = wordRecords.map(record => record.word);
+    const paragraphWords = words.join(' ');
+    res.status(200).send(paragraphWords);
+  })
+  .catch((err) => {
+    res.sendStatus(500);
+  })
+})
 
 db.sync()  // sync our database
   .then(function(){
